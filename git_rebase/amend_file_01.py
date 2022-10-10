e0c671df7ef3edc632bb21c9600cf5b035da76a1
olaf <olaf@buzzni.com>
2022-10-10 20:13:19 +0900

    test for rebase third trial succeed

--- git_rebase/amend_file_01.py
+++ git_rebase/amend_file_01.py
@@ -1,2 +1,3 @@
 # this is amend file 01
 # this is test for rebase
+# rebase sixth time succeed
--- git_rebase/amend_file_03.py
+++ git_rebase/amend_file_03.py
@@ -1,3 +1,6 @@
 # this is amend file 02 added
 # amend file 03 amended
 # amened file just changed
+# test for rebase 03
+
+# rebase fifth time succeed
