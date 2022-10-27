557cd0748174412af367eefbb37dc1aec09fd69a
Changyoungchan <san0558@naver.com>
2022-10-27 20:25:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-import requests
-from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
-response = requests.get(url)
-response.encoding = 'utf-8'
-html = response.text
-
-soup = BeautifulSoup(html, 'html.parser')
-
-bookservices = soup.select('.title_text')
-for no, book in enumerate(bookservices, 1):
-    print(no, book.text.strip())
+def test_number():
+    a = 4
+    assert a == 4
+    
+    
+def test_second_number():
+    a = 5
+    assert a == 4
