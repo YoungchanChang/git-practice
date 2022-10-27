cbc0e730e8fcf2c5d59cefafae92859fa591c64c
Changyoungchan <san0558@naver.com>
2022-10-27 15:48:33 +0900

    Update test.py

--- test_code/test.py
+++ test_code/test.py
@@ -1,21 +1,21 @@
 import json
 
-if __name__ == "__main__":
-    import requests
-    from bs4 import BeautifulSoup
 
-    url = 'https://ridibooks.com/category/new-releases/2200'
-    response = requests.get(url)
-    response.encoding = 'utf-8'
-    html = response.text
+import requests
+from bs4 import BeautifulSoup
 
-    soup = BeautifulSoup(html, 'html.parser')
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
 
-    bookservices = soup.select('.title_text')
+soup = BeautifulSoup(html, 'html.parser')
 
-    my_json_list = []
-    for no, book in enumerate(bookservices, 1):
-        my_json_list.append({no: book.text.strip()})
+bookservices = soup.select('.title_text')
 
-    with open('data.json', "w", encoding="UTF-8-sig") as f_write:
-        json.dump(my_json_list, f_write, ensure_ascii=False, indent=4)
+my_json_list = []
+for no, book in enumerate(bookservices, 1):
+    my_json_list.append({no: book.text.strip()})
+
+with open('data.json', "w", encoding="UTF-8-sig") as f_write:
+    json.dump(my_json_list, f_write, ensure_ascii=False, indent=4)
